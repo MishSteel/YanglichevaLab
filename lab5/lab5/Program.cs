73a41e35c2167a83a75ba7f60ff36831938c9932
MishSteel <spammishsteel@gmail.com>
2025-04-23 10:11:12 +0300

    done

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Car
+{
+    // Поля класса
+    public int yearBuilt;           // Год выпуска
+    public double maximumSpeed;     // Максимальная скорость
+
+    // Метод для вычисления возраста машины
+    public int Age(int currentYear)
+    {
+        int age = currentYear - yearBuilt;
+        return age;
+    }
+
+    // Метод для вычисления расстояния, пройденного автомобилем
+    public double Distance(double initialSpeed, double time)
+    {
+        return (initialSpeed + maximumSpeed) / 2 * time;
+    }
+}
+
+class RedCar
+{
+    public static void Main()
+    {
+        Console.WriteLine("Создание объекта типа Car и присвоение его адреса ссылке redPorsche\n");
+
+        // Создание объекта
+        Car redPorsche = new Car();
+
+        // Присвоение значений
+        redPorsche.yearBuilt = 2009;
+        redPorsche.maximumSpeed = 285;
+
+        // Вызов метода Age
+        int age = redPorsche.Age(2025);
+        Console.WriteLine("redPorsche имеет возраст " + age + " лет.");
+
+        // Вызов метода Distance
+        double distance = redPorsche.Distance(31, 0.23);
+        Console.WriteLine("redPorsche проехал " + distance + " миль.");
+
+        Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+        Console.ReadKey();
+    }
+}
